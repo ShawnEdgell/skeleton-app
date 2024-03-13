6e56913693e4eb2c2f9ac1e899dc3824c060710a
shawn edgell <shawn_edgell@hotmail.com>
2024-03-13 03:59:04 -0400

    initial push

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import PopMenu from './PopMenu.svelte';
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">
+		<PopMenu />
+	</svelte:fragment>
+	<svelte:fragment slot="trail">(text)</svelte:fragment>
+</AppBar>
---
+++ src/lib/components/PopMenu.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { popup, LightSwitch } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+
+	const popupClick: PopupSettings = {
+		event: 'click',
+		target: 'popupClick',
+		placement: 'bottom',
+		closeQuery: '.btn'
+	};
+</script>
+
+<button class="btn bg-primary-hover-token variant-filled-900" use:popup={popupClick}>
+	<div class="flex items-center justify-center gap-1">
+		<span class="flex items-center">Explore</span>
+		<svg
+			fill="currentColor"
+			width="10px"
+			height="10px"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			style="margin-top: 0;"
+		>
+			<path
+				d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569l9 13z"
+			/>
+		</svg>
+	</div>
+</button>
+
+<div class="card p-4 max-w-sm" data-popup="popupClick">
+	<div class="grid grid-cols-1 gap-2">
+		<div class="flex justify-between">
+			<strong>Mode</strong>
+			<LightSwitch />
+		</div>
+		<hr class="!border-t-2" />
+		<a href="/" class="btn variant-filled" data-sveltekit-preload-data="hover">Home</a>
+		<a href="/documentation" class="btn variant-filled" data-sveltekit-preload-data="hover"
+			>Documentation</a
+		>
+		<a href="/login" class="btn variant-filled" data-sveltekit-preload-data="hover">Login</a>
+	</div>
+	<div class="arrow bg-surface-100-800-token"></div>
+</div>
